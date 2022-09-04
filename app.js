dd36b72ba04ab22f9e27618b64d266911c594567
Manoj <manoj@Manojs-MacBook-Pro.local>
2022-09-04 22:25:10 +0530

    fixeed bugs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let container = document.querySelector('.container');
 for(let n = 1 ; n<=151; n++){
     const divisionElement = document.createElement('div');
     const spanElement = document.createElement('span');
-    spanElement.innerHTML = `#${n}`
+    spanElement.innerHTML = `#${n}`;
     const pokemon = document.createElement('img');
     pokemon.src = `${baseURL}${n}.png`;
     divisionElement.classList.add('flexview')
